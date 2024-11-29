b28d565c32a395e750f4fff2fd1d7a88e2ccf3eb
dm-turing <deephan.m@turing.com>
2024-11-29 23:56:28 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
-
-func divide(x, y float64) (float64, error) {
-	if y == 0 {
-		return 0, errors.New("division by zero")
-	}
-	return x / y, nil
-}
+import "fmt"
 
 func main() {
-	result, err := divide(10, 0)
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Println("Result:", result)
-	}
+	fmt.Println("Does nothing..")
 }
